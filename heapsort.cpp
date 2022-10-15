a8cf2dea9dcc19a60cefa403acf5251b52af880b
Sonali <sonalibyahut0125@gmail.com>
2022-10-15 11:49:57 +0530

    added

---
+++ heapsort.cpp
@@ -0,0 +1,40 @@
+  
+  #include <iostream>
+  using namespace std; 
+  void heap(int arr[], int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+  
+    if (l < n && arr[l] > arr[largest])
+      largest = l;
+  
+    if (r < n && arr[r] > arr[largest])
+      largest = r;
+    if (largest != i) {
+      swap(arr[i], arr[largest]);
+      heap(arr, n, largest);
+    }
+  }
+  void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+      heap(arr, n, i);
+    for (int i = n - 1; i >= 0; i--) {
+      swap(arr[0], arr[i]);
+      heap(arr, i, 0);
+    }
+  }
+  int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+    	cin>>arr[i];
+	}
+    heapSort(arr, n);
+  
+    cout << "Heap Sorted array is \n";
+    for (int i = 0; i < n; ++i)
+      cout << arr[i] << " ";
+    cout << "\n";
+  }
